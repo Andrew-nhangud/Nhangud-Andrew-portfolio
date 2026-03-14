42ce8506836a24ec78cac9891f7cd1a23b670e6c
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-03-14 14:29:22 +0200

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles/global.css";
+import { Navbar } from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="box"></div>
+    </div>
+  );
+};
