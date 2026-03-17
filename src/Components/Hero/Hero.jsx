96e6c6dd08c0ef8c1514e8e387c4602ef39277d6
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-03-17 15:26:55 +0200

    feat: hero section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import "./Hero.css";
+
+export const Hero = () => {
+  return (
+    <section id="Hero">
+      <div className="hero inner-contianer ">
+        <p>PORTFOLIO & CURRICULUM VITAE</p>
+        <h1>ANDREW NHANGUD</h1>
+
+        <div className="divider">
+          <div className="line"></div>
+          <div className="box"></div>
+          <div className="line"> </div>
+        </div>
+
+        <h2>Front-End Developer & Interface Designer</h2>
+        <p>
+          Crafting digital experiences with the precision and care of a
+          Michelin-starred kitchen.
+        </p>
+      </div>
+      <div className="section-end" />
+    </section>
+  );
+};
