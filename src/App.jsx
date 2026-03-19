54542ebe9264a44edd9e83c57cbcffa0458f53a8
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-03-19 15:07:49 +0200

    Added new components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Navbar } from "./Components/Navbar/Navbar";
 import { Hero } from "./Components/Hero/Hero";
 import { Skills } from "./Components/Skill/Skills";
 import { Project } from "./Components/Project/Project";
+import { About } from "./Components/About/About";
+import { Contact } from "./Components/Contact/Contact";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Hero />
         <Skills />
         <Project />
+        <About />
+        <Contact />
       </main>
     </div>
   );
