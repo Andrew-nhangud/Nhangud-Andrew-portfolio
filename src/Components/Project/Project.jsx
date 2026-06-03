999d3da1945050112975f9a569bdebb73c59b313
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-06-03 15:26:15 +0200

    updated the description for the project

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -21,7 +21,7 @@ const projectLists = [
   {
     label: "kanban",
     description:
-      "A responsive, client side task management application designed for seamless daily workflow organization. The application enables users to dynamically track tasks across structured workflows while ensuring data persistence and cross device accessibility.",
+      "A responsive, client side task management application designed for seamless daily workflow organization. The application enables users to dynamically track tasks across structured workflows while ensuring data persistence and cross gitdevice accessibility.",
     skills: ["JavaScript", "CSS", "HTML"],
     link: "https://andnha-25109-fto-2502a-group-a1-and.vercel.app",
   },
